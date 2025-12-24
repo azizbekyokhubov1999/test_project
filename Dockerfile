14979800e079195059a34658d5e4ade97a023771
azizbekyokhubov1999 <yoqubov.azizbek.99@gmail.com>
2025-12-24 16:45:12 +0500

    Fix repackeging error - 10

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@
 FROM maven:3.8.5-eclipse-temurin-17 AS build
 WORKDIR /app
 COPY pom.xml .
-COPY src ./src
+COPY src1 ./src
 RUN mvn clean package -DskipTests
 
 # Stage 2: Runtime
---
+++ src/main/java/com/example/HelloDevOps.java
@@ -0,0 +1,11 @@
+package com.example; // This MUST match the folder path com/example
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloDevOps {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloDevOps.class, args);
+    }
+}
