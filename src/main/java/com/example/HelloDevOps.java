d56deb2200729125669836a80f96a4fd7049b3e6
azizbekyokhubov1999 <yoqubov.azizbek.99@gmail.com>
2025-12-25 14:49:59 +0500

    Fix repackeging error - 16

--- src/main/java/com/example/HelloDevOps.java
+++ src/main/java/com/example/HelloDevOps.java
@@ -1,11 +1,23 @@
-package com.example; // This MUST match the folder path com/example
+package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class HelloDevOps {
     public static void main(String[] args) {
+        System.out.println("Hello and welcome!");
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
         SpringApplication.run(HelloDevOps.class, args);
+    }
+
+    @GetMapping("/")
+    public String health() {
+        return "Application is Healthy!";
     }
 }
